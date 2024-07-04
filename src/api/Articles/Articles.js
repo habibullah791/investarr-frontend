a7e81eae36a9d3aefe73175057c975dcd22f5cdc
Habib ullah <habibullahshahid001@gmail.com>
2024-07-05 01:19:20 +0500

    Update: api url

--- src/api/Articles/Articles.js
+++ src/api/Articles/Articles.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'https://web-production-15a27.up.railway.app/api';
-const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = 'https://web-production-15a27.up.railway.app/api';
+// const API_URL = 'http://127.0.0.1:8000/api';
 
 export const fetchArticles = async (accessToken) => {
     try {
